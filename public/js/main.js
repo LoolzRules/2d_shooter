62bbb9bd0ff656c05f22561595d922d8ec9ec2c7
LoolzRules <loolzrules@gmail.com>
2018-09-23 17:50:12 +0600

    Added basic physics and walls. Reworked scene architecture

--- public/js/main.js
+++ public/js/main.js
@@ -11,10 +11,12 @@ window.onload = function () {
 
             this.container = scene.add.container( x, y );
             this.container.add( [this.body, this.rifle, this.head] );
-            this.container.setSize( 72, 72 );
+
             scene.physics.world.enable( this.container );
-            this.container.body.setCircle(36);
-            this.container.body.setMaxVelocity( this.maxSpeed ).setCollideWorldBounds( true );
+            this.container.body
+                .setCircle( 36, -36, -36 )
+                .setMaxVelocity( this.maxSpeed )
+                .setCollideWorldBounds( true );
         }
 
         update( w, a, s, d, x, y ) {
@@ -48,102 +50,216 @@ window.onload = function () {
         }
     }
 
-    const preload = function () {
-        this.load.setBaseURL( 'http://localhost:3000' );
-        this.load.image( 'body', 'assets/pose_1_heavy.svg' );
-        this.load.image( 'rifle', 'assets/assault_rifle.svg' );
-        this.load.image( 'head', 'assets/gas.svg' );
-        this.load.image( 'map', 'assets/bg-1.jpg' );
-    };
+    class MainScene extends Phaser.Scene {
+        constructor() {
+            super( {
+                key: 'mainScene',
+                active: true,
+                physics: {
+                    default: 'arcade',
+                    arcade: {
+                        debug: true
+                    }
+                },
+            } );
 
+            this.maincamSize = window.innerHeight;
+            this.maincamOffset = (window.innerWidth - window.innerHeight) / 2;
 
-    const create = function () {
-        this.add.image( 0, 0, 'map' ).setOrigin( 0.5 );
-        this.physics.world.setBounds( -500, -500, 1000, 1000, true, true, true, true );
+            this.boundX = -900;
+            this.boundY = -600;
+            this.boundW = -2 * this.boundX;
+            this.boundH = -2 * this.boundY;
 
-        this.setupMainCamera( this.cameras.main );
+            this.assets = [
+                {
+                    "key": "iw_1",
+                    "path": "/assets/inner_walls/1.svg",
+                    "x": 494,
+                    "y": 888,
+                    "angle": 0
+                },
+                {
+                    "key": "iw_2",
+                    "path": "/assets/inner_walls/2.svg",
+                    "x": 317,
+                    "y": 888,
+                    "angle": 0
+                },
+                {
+                    "key": "iw_3",
+                    "path": "/assets/inner_walls/3.svg",
+                    "x": 584,
+                    "y": 888,
+                    "angle": 0
+                },
+                {
+                    "key": "iw_4",
+                    "path": "/assets/inner_walls/4.svg",
+                    "x": 305,
+                    "y": 888,
+                    "angle": 0
+                },
+                {
+                    "key": "iw_5",
+                    "path": "/assets/inner_walls/5.svg",
+                    "x": 357,
+                    "y": 620,
+                    "angle": 0
+                },
+                {
+                    "key": "iw_6",
+                    "path": "/assets/inner_walls/6.svg",
+                    "x": 1062,
+                    "y": 740,
+                    "angle": 0
+                },
+                {
+                    "key": "iw_7",
+                    "path": "/assets/inner_walls/7.svg",
+                    "x": 596,
+                    "y": 728,
+                    "angle": 0
+                },
+                {
+                    "key": "iw_8",
+                    "path": "/assets/inner_walls/8.svg",
+                    "x": 1229,
+                    "y": 728,
+                    "angle": 0
+                },
+                {
+                    "key": "iw_9",
+                    "path": "/assets/inner_walls/9.svg",
+                    "x": 768,
+                    "y": 728,
+                    "angle": 0
+                },
+                {
+                    "key": "iw_10",
+                    "path": "/assets/inner_walls/10.svg",
+                    "x": 584,
+                    "y": 336,
+                    "angle": 0
+                },
+                {
+                    "key": "iw_11",
+                    "path": "/assets/inner_walls/11.svg",
+                    "x": 512,
+                    "y": 608,
+                    "angle": 0
+                },
+                {
+                    "key": "iw_12",
+                    "path": "/assets/inner_walls/12.svg",
+                    "x": 84,
+                    "y": 608,
+                    "angle": 0
+                }
+            ]
 
-        this.player = new Player( this, 0, 0 );
+        }
 
-        this.cameras.main.startFollow( this.player.container, false, 0.1, 0.1 );
+        preload() {
+            this.load.setBaseURL( 'http://localhost:3000' );
+            this.load.image( 'body', 'assets/pose_1_heavy.svg' );
+            this.load.image( 'rifle', 'assets/assault_rifle.svg' );
+            this.load.image( 'head', 'assets/gas.svg' );
+            this.load.image( 'map', 'assets/bg-1.jpg' );
+            this.assets.forEach( ( asset ) => {
+                this.load.image( asset.key, asset.path );
+            } );
+        };
 
-        this.minimap = this.cameras.add( 900, 0, 200, 200 );
-        this.setupMinimap( this.minimap );
+        create() {
 
-        this.controls.w = this.input.keyboard.addKey( Phaser.Input.Keyboard.KeyCodes.W );
-        this.controls.a = this.input.keyboard.addKey( Phaser.Input.Keyboard.KeyCodes.A );
-        this.controls.s = this.input.keyboard.addKey( Phaser.Input.Keyboard.KeyCodes.S );
-        this.controls.d = this.input.keyboard.addKey( Phaser.Input.Keyboard.KeyCodes.D );
-        this.controls.cursorX = 0;
-        this.controls.cursorY = 0;
+            this.map = this.add.group();
 
-        this.input.on( 'pointermove', function ( pointer ) {
-            this.controls.cursorX = pointer.x;
-            this.controls.cursorY = pointer.y;
-        }, this );
-    };
+            this.assets.forEach( ( asset ) => {
+                let obj = this.add.image(
+                    asset.x + this.boundX,
+                    asset.y + this.boundY,
+                    asset.key )
+                    .setOrigin( 0 )
+                    .setRotation( asset.angle / 180 * Math.PI );
+                this.physics.world.enable( obj );
+                obj.body.immovable = true;
+                this.map.add( obj );
+            } );
 
-    const update = function () {
-        this.player.update(
-            this.controls.w.isDown,
-            this.controls.a.isDown,
-            this.controls.s.isDown,
-            this.controls.d.isDown,
-            this.controls.cursorX + this.cameras.main.scrollX,
-            this.controls.cursorY + this.cameras.main.scrollY
-        );
-    };
+            // Enabling physics
+            this.physics.world.setBounds(
+                this.boundX, this.boundY,
+                this.boundW, this.boundH,
+                true, true, true, true );
 
-    const setupMainCamera = function ( camera ) {
-        camera
-            .setName( 'main' )
-            .setBounds( -500, -500, 1000, 1000 )
-            .setSize( window.innerHeight, window.innerHeight )
-            .setPosition( (window.innerWidth - window.innerHeight) / 2, 0 )
-            .setBackgroundColor( 0x444444 );
-    };
+            // Adding player
+            this.player = new Player( this,
+                this.boundX + this.boundW / 2,
+                this.boundY + this.boundH / 2 );
 
-    const setupMinimap = function ( minimap ) {
-        let size = (window.innerWidth - window.innerHeight) / 2;
-        minimap
-            .setName( 'mini' )
-            .setZoom( 0.1 )
-            .setSize( size, size )
-            .setPosition( window.innerWidth - size, 0 )
-            .setBackgroundColor( 0x002244 );
-    };
+            // Setting up main camera
+            this.setupMainCamera();
+
+            // Adding minimap
+            this.minimap = this.cameras.add();
+            this.setupMinimap();
+
+            // Adding input
+            this.controls = {
+                w: this.input.keyboard.addKey( Phaser.Input.Keyboard.KeyCodes.W ),
+                a: this.input.keyboard.addKey( Phaser.Input.Keyboard.KeyCodes.A ),
+                s: this.input.keyboard.addKey( Phaser.Input.Keyboard.KeyCodes.S ),
+                d: this.input.keyboard.addKey( Phaser.Input.Keyboard.KeyCodes.D ),
+                cursorX: 0,
+                cursorY: 0,
+            };
+
+            this.input.on( 'pointermove', function ( pointer ) {
+                this.controls.cursorX = pointer.x;
+                this.controls.cursorY = pointer.y;
+            }, this );
+        };
+
+        update() {
+            this.player.update(
+                this.controls.w.isDown,
+                this.controls.a.isDown,
+                this.controls.s.isDown,
+                this.controls.d.isDown,
+                this.controls.cursorX + this.cameras.main.scrollX,
+                this.controls.cursorY + this.cameras.main.scrollY
+            );
+            this.physics.world.collide( this.player.container, this.map );
+        };
+
+        setupMainCamera() {
+            this.cameras.main
+                .setName( 'main' )
+                .setBounds( this.boundX, this.boundY, this.boundW, this.boundH )
+                .setSize( this.maincamSize, this.maincamSize )
+                .setPosition( this.maincamOffset, 0 )
+                .setBackgroundColor( 0x444444 )
+                .startFollow( this.player.container, false, 0.1, 0.1 );
+        };
+
+        setupMinimap() {
+            this.minimap
+                .setName( 'mini' )
+                .setZoom( 0.1 )
+                .setSize( this.maincamOffset, this.maincamOffset )
+                .setPosition( window.innerWidth - this.maincamOffset, 0 )
+                .setBackgroundColor( 0x002244 );
+        };
+
+    }
 
     const config = {
         type: Phaser.AUTO,
         width: document.body.scrollWidth,
         height: document.body.scrollHeight,
         backgroundColor: '#000000',
-        physics: {
-            default: 'arcade',
-            arcade: {
-                debug: true
-            }
-        },
-        scene: {
-            preload,
-            create,
-            update,
-            extend: {
-                maincam: null,
-                minimap: null,
-                player: null,
-                controls: {
-                    w: null,
-                    a: null,
-                    s: null,
-                    d: null,
-                    cursorX: null,
-                    cursorY: null,
-                },
-                setupMainCamera,
-                setupMinimap,
-            }
-        },
+        scene: [MainScene],
         fps: 30,
     };
 
